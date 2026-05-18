c548996a423714dc22f2c97db8b49f047f67e635
xulimin <xlm_fy@126.com>
2026-05-18 16:53:08 +0800

    aigc

--- tj-aigc/src/main/java/com/tianji/aigc/config/SpringAIConfig.java
+++ tj-aigc/src/main/java/com/tianji/aigc/config/SpringAIConfig.java
@@ -12,8 +12,6 @@ import org.springframework.ai.chat.client.advisor.api.Advisor;
 import org.springframework.ai.chat.memory.ChatMemory;
 import org.springframework.ai.chat.memory.ChatMemoryRepository;
 import org.springframework.ai.chat.memory.MessageWindowChatMemory;
-
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
